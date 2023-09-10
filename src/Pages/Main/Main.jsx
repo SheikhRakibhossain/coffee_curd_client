6a19e621938509fdc696f1b8e24949f564d48c77
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-10 20:52:55 +0600

    just test for runing web app

---
+++ src/Pages/Main/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "../../Component/Header/Header";
+
+const Main = () => {
+    return (
+        <>
+            <Header/>
+            <Outlet/>
+        </>
+    );
+};
+
+export default Main;
