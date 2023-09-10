0fbb845bb9a37213aee55bcd1193f0a95574919b
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-10 21:33:43 +0600

    coffee page create

---
+++ src/Pages/AddCoffee/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+const AddCoffee = () => {
+    return (
+        <>
+        <h1>I am add coffee page.</h1>
+            
+        </>
+    );
+};
+
+export default AddCoffee;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,17 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../Pages/Main/Main";
+import AddCoffee from "../Pages/AddCoffee/AddCoffee";
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main/>,
-    },
+      children:[
+        {
+            path:'addcoffee',
+            element:<AddCoffee/>
+        }
+      ]
+    }
   ]);
   export default router;
